d3b1b3d795d607a913ff1d7f209e8ee91edd10d8
MichelML <michmoreau.l@gmail.com>
2016-09-13 09:21:22 -0400

    chore:add core modules and env variables

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-var express = require('express');
-var app = express();
-var morgan = require('morgan');
-var indexView = require('./routes/index');
-var signinView = require('./routes/signin');
-var eventView = require('./routes/event');
-var profileView = require('./routes/profile');
-var eventSumView = require('./routes/eventsummary');
-var signupView = require('./routes/signup');
+// Set Environment
+require('dotenv').config();
+
+// Modules
+const express = require('express');
+const EventEmitter = require('events');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const methodOverride = require('method-override');
+const simpleGet = require('simple-get');
+const morgan = require('morgan');
+const errorhandler = require('errorhandler');
+const gapi = require('./lib/gapi');
+
+// Views
+const indexView = require('./routes/index');
+const signinView = require('./routes/signin');
+const eventView = require('./routes/event');
+const profileView = require('./routes/profile');
+const eventSumView = require('./routes/eventsummary');
+const signupView = require('./routes/signup');
+
+// App settings and middlewares
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    // only use in development
+    app.use(errorhandler());
+}
 
 app.set('port', (process.env.PORT || 5000));
-
-app.use(morgan('combined'));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is the directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(express.static(__dirname + '/public'));
+
+// Set views 
 app.use('/', indexView);
 app.use('/', signinView);
 app.use('/', signupView);
@@ -25,4 +45,4 @@ app.use('/', eventView);
 app.use('/', profileView);
 app.use('/', eventSumView);
 
-app.listen(app.get('port'),() => console.log('Node app is running on port', app.get('port')););
+app.listen(app.get('port'), () => console.log('Node app is running on port', app.get('port')));
