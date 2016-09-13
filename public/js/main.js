9cb3bebf0153d66a4dccfabfd3514d0016a4a7e4
MichelML <michmoreau.l@gmail.com>
2016-09-13 10:47:19 -0400

    feat:add animation to google signin button

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,20 @@
-  function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-
-    //hide button
-    //$("#g-signin2").hide();
-  }
-
- function renderButton() {
-    gapi.signin2.render('g-signin2', {
-      'scope': 'profile email',
-      'width':240,
-      'height': 50,
-      'longtitle':true,
-      'theme': 'light',
-      'onsuccess': onSignIn
-      // 'onfailure': onFailure
-    });
- }
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-}
-
 $(".button-collapse").sideNav();
 
 $(document).ready(function() {
-  $('#email-account').focus();
+  var $emailAccount = $('#email-account'),
+      $signinLink = $('#signin-link'),
+      $googleButton = $('#google-btn');
+  //$emailAccount.focus();
+  $signinLink.focus(function() {
+    $googleButton.attr('src', 'img/btn_google_signin_pressed.png');
+  });
+  $signinLink.hover(function() {
+     $googleButton.attr('src', 'img/btn_google_signin_focus.png');
+  });
+  $signinLink.on('focusout, mouseout', function() {
+     $googleButton.attr('src', 'img/btn_google_signin_normal.png');
+  });
 });
-
 
 $(document).ready(function(event) {
     $('#name-event').focus();
