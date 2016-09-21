ed1ab82b2b8f6679b069f13ab6d0da68bda777ec
MichelML <michmoreau.l@gmail.com>
2016-09-21 12:05:23 -0400

    refactor:use html5 autofocus

--- public/js/eventmain.js
+++ public/js/eventmain.js
@@ -17,8 +17,6 @@ $(document).ready(function() {
 });
 
 $(document).ready(function(event) {
-    $('#name-event').focus();
-
     $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 10, // Creates a dropdown of 15 years to control year
