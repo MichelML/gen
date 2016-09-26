04b1ef650f627831838519aa3685434623a0c965
MichelML <michmoreau.l@gmail.com>
2016-09-25 21:31:48 -0400

    refactor:transfer validation functions to a module

--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    validate = require('../lib/validation.js');
 
 app.get('/signup', function(request, response) {
     response.render('./app/blocks/signup');
@@ -10,16 +11,3 @@ app.post('/signup', function(request, response) {
 });
 
 module.exports = app;
-
-
-function isValidEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email.value);
-}
-
-function isValidPassword(pw) {
-    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/.test(pw.value);
-}
-
-function isPasswordConfirmMatching(pw1, pw2) {
-    return pw1.value === pw2.value;
-}
