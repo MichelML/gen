bb798847e7502519b8236acb8dbd4fe049d82f70
MichelML <michmoreau.l@gmail.com>
2016-09-25 21:15:08 -0400

    docs:delete unnecessary comments in code

--- routes/signup.js
+++ routes/signup.js
@@ -2,8 +2,6 @@ var express = require('express'),
     app = express();
 
 app.get('/signup', function(request, response) {
-    //if loggedin, redirect to event creation
-    //else stay on page
     response.render('./app/blocks/signup');
 });
 
