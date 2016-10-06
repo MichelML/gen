4e186b8b51ac428b2c0bf0b166b864a60bed405a
MichelML <michmoreau.l@gmail.com>
2016-10-06 17:22:48 -0400

    feat: implement password hashing

--- routes/signin.js
+++ routes/signin.js
@@ -1,10 +1,13 @@
 var express = require('express'),
     app = express(),
     validate = require('../lib/validation.js')
-    usersTable = require('../models/users.js');
+    usersTable = require('../models/users.js'),
+    bcrypt = require('bcrypt-nodejs');
 
 app.get('/signin', function(request, response) {
+
     response.render('./app/blocks/signin');
+
 });
 
 app.post('/signin', (request, response) => {
@@ -12,21 +15,36 @@ app.post('/signin', (request, response) => {
     reqBody = request.body;
     usersTable.find(reqBody['email-account'])
         .then(user=>{
-            console.log(user.image + ' <- pw of user');
-            if (!validate.isValidEmail(reqBody['email-account']) || 
+
+            if (user.googlelogin) {
+                
+                locals.error = 'Please use Google Sign-In';
+                response.render('app/blocks/signin', locals);
+
+            }
+
+            else if (!validate.isValidEmail(reqBody['email-account']) || 
                 !validate.isValidPassword(reqBody['password-account']) || 
-                !validate.isPasswordConfirmMatching(reqBody['password-account'], user.pw)) {
+                !bcrypt.compareSync(reqBody['password-account'], user.pw)) {
+
                 locals.error = 'wrong email or password';
                 response.render('app/blocks/signin', locals);
+
             }
+
             else {
+
                 app.locals.me = user;
                 response.render('app/blocks/event');
+
             }
+
         })
         .catch(error=>{
+
             locals.error = 'user does not exist';
             response.render('app/blocks/signin', locals);
+
         });
 });
 
--- routes/signup.js
+++ routes/signup.js
@@ -3,47 +3,76 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     validate = require('../lib/validation.js'),
     usersTable = require('../models/users.js'),
+    bcrypt = require('bcrypt-nodejs'),
     reqBody = {};
 
 
 app.get('/signup', (request, response) => {
+
     response.render('./app/blocks/signup');
+
 });
 
 app.post('/signup', (request, response) => {
+
     var locals = {};
     reqBody = request.body;
+
     if (!validate.isValidEmail(reqBody['email-account'])) {
-        locals.error = 'invalid email'; 
-        response.render('app/blocks/signup',locals);
+
+        locals.error = 'invalid email';
+        response.render('app/blocks/signup', locals);
+
     } 
+
     else if (!validate.isValidPassword(reqBody['password-account'])) {
-        locals.error = 'invalid password'; 
-        response.render('app/blocks/signup',locals);
+
+        locals.error = 'invalid password';
+        response.render('app/blocks/signup', locals);
+
     } 
+
     else if (!validate.isValidPassword(reqBody['password2-account']) ||
-        !validate.isValidPassword(reqBody['password-account'],reqBody['password2-account'])) {
-        locals.error = 'invalid password confirmation'; 
-        response.render('app/blocks/signup',locals);
-    }
+
+        !validate.isPasswordConfirmMatching(reqBody['password-account'], reqBody['password2-account'])) {
+        locals.error = 'invalid password confirmation';
+        response.render('app/blocks/signup', locals);
+
+    } 
+
     else {
-         var tempName = reqBody['email-account'].replace(/@.+/, '');
-         app.locals.me = {
-                displayname: tempName,
-                firstname: tempName,
-                lastname: tempName,
-                pw: reqBody['password-account'],
-                googlelogin: false,
-                image: 'img/gen-green.png',
-                imagebig: 'img/gen-green.png',
-                email: reqBody['email-account'],
-                contacts: [reqBody['email-account']]
-            };
-         usersTable.add(app.locals.me)
-             .then(data=>{
-                response.render('app/blocks/event');
-             });
+
+        var tempName = reqBody['email-account'].replace(/@.+/, '');
+        app.locals.me = {
+            displayname: tempName,
+            firstname: tempName,
+            lastname: tempName,
+            image: 'img/gen-green.png',
+            imagebig: 'img/gen-green.png',
+            email: reqBody['email-account'],
+            contacts: [reqBody['email-account']]
+        };
+
+        var pgObject = {};
+        pgObject.pw = bcrypt.hashSync(reqBody["password-account"]);
+        pgObject.googlelogin = false;
+        Object.setPrototypeOf(pgObject, app.locals.me);
+
+        usersTable.add(pgObject)
+
+        .then(data => {
+            response.render('app/blocks/event');
+        })
+
+        .catch(() => {
+            
+            locals.error = 'Oops! Something went wrong<br>Please try again!';
+            response.render('app/blocks/signup', locals);
+
+        });
+
     }
+
 });
 
 module.exports = app;
