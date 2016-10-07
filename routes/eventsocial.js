07a825726f5c6ebd2dd1d467197168e8b124ddfc
MichelML <michmoreau.l@gmail.com>
2016-10-07 15:56:41 -0400

    refactor: change name of route and view for event creation form

--- routes/eventsocial.js
+++ routes/eventsocial.js
@@ -1,11 +1,11 @@
 var express = require('express'),
     app = express();
 
-app.get('/event', function(request, response) {
+app.get('/eventsocial', function(request, response) {
     //if a user is not logged in, redirect to authentication
     //  to be done
     //else stay on the page
-    response.render('./app/blocks/event');
+    response.render('./app/blocks/eventsocial');
 });
 
 module.exports = app;
