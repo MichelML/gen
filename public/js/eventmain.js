495f4aa5fa0572f96fce71fc364b67d4583ff881
MichelML <michmoreau.l@gmail.com>
2016-10-12 09:21:48 -0400

    refactor: place date and time picker init in eventmain script

--- public/js/eventmain.js
+++ public/js/eventmain.js
@@ -1,3 +1,21 @@
+$(document).ready(function(event) {
+
+    $('.datepicker').pickadate({
+
+        selectMonths: true,
+        selectYears: 10,
+        format: 'dd-mm-yyyy'
+
+    });
+
+    $('.timepicker').pickatime({
+
+        interval: 15
+
+    });
+
+});
+
 function initAutocomplete() {
 
     var input = document.getElementById('place-event');
