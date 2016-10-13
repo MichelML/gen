ba4d1d1313db059e3c4c06476905b188515ff55a
MichelML <michmoreau.l@gmail.com>
2016-10-13 08:41:17 -0400

    refactor: rename function name for form validation lib

--- routes/eventpersonal.js
+++ routes/eventpersonal.js
@@ -10,7 +10,7 @@ app.get('/eventpersonal', (request, response) => {
 
 app.post('/eventpersonal', (request, response) => {
 
-    var reqBody = request.body;
+    var form = request.body;
     response.send('success'); 
 
 });
