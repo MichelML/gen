d72a9c1af5786228ac4691b6d31059742cf48442
MichelML <michmoreau.l@gmail.com>
2016-10-13 10:26:38 -0400

    feat: complete sql methods for events transactions

--- models/events.js
+++ models/events.js
@@ -20,35 +20,62 @@ var pgp = pgpLib(options); // initializing pg-promise;
 var db = pgp("postgres://mimolap@localhost:5432/gen");
 
 // Events repository
-function userActions(obj) {
+function eventsActions(obj) {
     return {
         addPersonalEvent: function (form) {
-            return obj.one('INSERT INTO events VALUES(DEFAULT,$1,current_date,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12) RETURNING eventname', [form['type'], form['']])
-                .then(eventform=> {
-                    return JSON.parse(data.contacts);
-                });
+            return obj.one('INSERT INTO events VALUES(DEFAULT,$1,current_date,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16) RETURNING eventname', 
+                    [form['user'],
+                    form['type'], 
+                    form['name-event'], 
+                    form['type-event'], 
+                    form['guests-event'], 
+                    // a personal event does not have a host and guests information
+                    // those entries are therefore empty
+                    '', '', '', '', '', 
+                    form['place-event'], 
+                    form['startdate-event'], 
+                    form['starttime-event'], 
+                    form['enddate-event'], 
+                    form['endtime-event'], 
+                    form['details-event']]
+                    );
         },
-        // Get number of active users
-        count: function () {
-            return obj.one("SELECT count(*) FROM users")
-                .then(data=> {
-                    return data.count;
-                });
+
+        addSocialEvent: function (form) {
+            return obj.one('INSERT INTO events VALUES(DEFAULT,$1,current_date,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16)', 
+                    [form['user'],
+                    form['type'], 
+                    form['name-event'], 
+                    form['type-event'], 
+                    form['guests-event'], 
+                    // a personal event does not have a host and guests information
+                    // those entries are therefore empty
+                    form['host-event'], 
+                    form['tel-event'], 
+                    form['mail-event'], 
+                    form['link-event'], 
+                    form['guests-event'], 
+                    form['place-event'], 
+                    form['startdate-event'], 
+                    form['starttime-event'], 
+                    form['enddate-event'], 
+                    form['endtime-event'], 
+                    form['details-event']]
+                    );
         },
-        find: function (email) {
-            return obj.one("SELECT firstname,lastname,displayname,email,image,imagebig,contacts from users WHERE email = $1", email)
-                .then(data=> {
-                    return data
-                });
+
+        findAll: function (email) {
+            return obj.one("SELECT eventname,eventstartdate,eventstarttime FROM events WHERE createdby = $1", email);
         },
-        // Delete users by name and return the number of users deleted
-        delete: function (email) {
-            return obj.result("DELETE FROM users WHERE email=$1", email)
-                .then(result=> {
-                    return result.rowCount;
-                });
+
+        find: function (id) {
+            return obj.one("SELECT * FROM events WHERE SERIAL = $1", id);
+        },
+
+        delete: function (id) {
+            return obj.result("DELETE FROM events WHERE id=$1", id);
         }
-    }
+    };
 }
 
-module.exports = db.users;
+module.exports = db.events;
