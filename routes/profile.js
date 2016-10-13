af8bc2da4357fa4bf8763395a37c13fee521f7dd
MichelML <michmoreau.l@gmail.com>
2016-10-13 11:08:52 -0400

    refactor: delete obsolete comments

--- routes/profile.js
+++ routes/profile.js
@@ -2,8 +2,6 @@ var express = require('express'),
     app = express();
 
 app.get('/me', function(request, response) {
-//if a user is not loggedin, redirect to auth page
-//else stay on the page
   response.render('./app/blocks/profile');
 });
 
