1036ba7a4c79934debac448c806a6dc568f6cd9d
MichelML <michmoreau.l@gmail.com>
2016-10-13 08:39:35 -0400

    refactor: move event form validation methods to new lib

--- routes/eventpersonal.js
+++ routes/eventpersonal.js
@@ -1,28 +1,18 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    validator = require('../lib/formvalidation.js');
 
 app.get('/eventpersonal', (request, response) => {
+
     response.render('./app/blocks/eventpersonal');
+
 });
 
 app.post('/eventpersonal', (request, response) => {
+
     var reqBody = request.body;
-    console.log(reqBody);
     response.send('success'); 
+
 });
 
 module.exports = app;
-
-
-function formIsValid(formdata) {
-
-    return  validator.checkEventName($eventName.val()) && 
-            validator.checkEventType($eventType.val()) &&
-            validator.checkEventLocation($eventLocation.val()) &&
-            validator.checkEventDate($eventStartDate.val()) && 
-            validator.checkEventTime($eventStartTime.val()) &&
-            validator.checkEventDate($eventEndDate.val()) && 
-            validator.checkEventTime($eventEndTime.val()) &&
-            validator.checkEventDetails($eventDetails.val());
-
-}
