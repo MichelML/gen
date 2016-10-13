795ab163938182002b4db1d550550d44c37476e6
MichelML <michmoreau.l@gmail.com>
2016-10-13 11:26:04 -0400

    refactor: remove obsolete comments

--- routes/eventsocial.js
+++ routes/eventsocial.js
@@ -1,11 +1,12 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    isEventFormValid = require('../lib/formvalidation.js').form.socialEventFormIsValid,
+    eventsTable = require('../models/db.js').events;
 
 app.get('/eventsocial', function(request, response) {
-    //if a user is not logged in, redirect to authentication
-    //  to be done
-    //else stay on the page
+
     response.render('./app/blocks/eventsocial');
+
 });
 
 module.exports = app;
