57b08c244d84babf62eb95813abfd0538906b1c5
MichelML <michmoreau.l@gmail.com>
2016-10-14 11:56:21 -0400

    fix: delete returning value in method inserting a new event

--- models/events.js
+++ models/events.js
@@ -2,7 +2,7 @@
 function eventsActions(obj) {
     return {
         addPersonalEvent: function (form) {
-            return obj.none('INSERT INTO events VALUES(DEFAULT,$1,current_date,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15) RETURNING eventname', 
+            return obj.none('INSERT INTO events VALUES(DEFAULT,$1,current_date,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15)', 
                     [form['user'],
                     form['type'], 
                     form['name-event'], 
