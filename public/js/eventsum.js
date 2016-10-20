88936031bb6ab7c4ea9718ba3f73b1d28c52d819
MichelML <michmoreau.l@gmail.com>
2016-10-19 22:37:33 -0400

    refactor: add spacing to code

--- public/js/eventsum.js
+++ public/js/eventsum.js
@@ -1,26 +1,39 @@
 function selectElementContents(el) {
+
     var range = document.createRange();
     range.selectNodeContents(el);
-    var sel = window.getSelection();
-    sel.removeAllRanges();
-    sel.addRange(range);
+
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+
 }
 
 var editableContent = document.querySelectorAll('.editable')
 
 editableContent.forEach(function(val,index) {
+
     editableContent[index].onfocus = function() {
+
         selectElementContents(editableContent[index]);
+
     };
+
 });
 
 
 function indicateHowToEdit() {
+
     Materialize.toast('Type to edit', 2000)
+
 }
 
 $('.editable').each(function(index) {
+
     $(this).on('focus', function () {
+
         indicateHowToEdit();
+
     });
+
 });
